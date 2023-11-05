ea0511828250c0d5d453c4684850fc0006694e5d
Aok <61693887+Aoook@users.noreply.github.com>
2023-11-05 17:28:26 +0100

    Added store and game pages

---
+++ pages/2_store.vue
@@ -0,0 +1,56 @@
+<template>
+    <div>
+        <!-- redirection to all the pages -->
+      <MenuComponent />
+      <h1 class="d-flex justify-content-center h1">we are at "the store" page</h1>
+
+
+      <div>
+      <br/><h2 class="flex justify-center h2">sellable items:</h2><br/>
+      <div v-for="(price, weaponName) in playerData.weapon" :key="weaponName">
+        <p>{{ weaponName }}: {{ price }}€</p>
+      </div>
+      </div>
+
+      <div>
+        <br/><br/><h2 class="flex justify-center h2">buyable items:</h2><br/>
+        <p>weapon4: 50€</p>
+        <p>weapon5: 60€</p>
+      </div>
+
+    </div>
+
+    <footer>
+
+      <p>order list</p>
+      <ol>
+        <li>a</li>
+        <li>b</li>
+        <li>c</li>
+      </ol>
+
+      <p>unorder list</p>
+      <ul>
+        <li>a</li>
+        <li>b</li>
+        <li>c</li>
+      </ul>
+
+    </footer>
+  </template>
+  
+  <script>
+  import MenuComponent from '../components/menu.vue';
+  import playerData from '../data/player.json';
+  
+  export default {
+    components: {
+      MenuComponent
+    },
+    data() {
+    return {
+      playerData: playerData
+    };
+  }
+  };
+  </script>
---
+++ pages/3_game.vue
@@ -0,0 +1,56 @@
+<template>
+    <div>
+        <!-- redirection to all the pages -->
+      <MenuComponent />
+      <h1 class="d-flex justify-content-center h1">we are at "looking for game" page</h1>
+
+      <div class=""> <!-- game creation + finder -->
+      <div class="grid-container">
+        <div clas="grid-item"> <!-- game creation -->
+          <h2 class="flex justify-center h2">Create a game</h2><br/><br/>
+
+          <p>number of rows:</p>
+          <input value="10" class="form-control"/><br/><br/>
+
+          <p>number of columns:</p>
+          <input value="10" class="form-control"/><br/><br/>
+
+          <p>player HP:</p>
+          <input value="5" class="form-control"/><br/><br/>
+
+          <router-link class="navbar-brand btn btn-light" to="/page3_1">create new game</router-link>
+        </div>
+
+        <div class="grid-item bg-cyan-100"> <!-- game finder -->
+          <h2 class="flex justify-center h2">Game finder</h2><br/><br/>
+
+          <div>
+            <p>Lobby 1, waiting for player</p>
+            <button class="btn btn-primary">Join</button>
+          </div><br/>
+
+          <div>
+            <p>Lobby 2, waiting for player</p>
+            <button class="btn btn-primary">Join</button>
+          </div><br/>
+
+          <div>
+            <p>Lobby 3, finish</p>
+            <button class="btn btn-primary">View stat</button>
+          </div><br/>
+      </div>
+    </div>
+    </div>
+
+    </div>
+  </template>
+  
+  <script>
+  import MenuComponent from '../components/menu.vue';
+  
+  export default {
+    components: {
+      MenuComponent
+    }
+  };
+  </script>
